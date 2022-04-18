d9a7a446bfe88d05e10605a5dc15274ecf444b72
yavuzcirit <yavuzcirit23@gmail.com>
2022-04-18 09:01:15 +0300

    README file updated

--- README.md
+++ README.md
@@ -20,21 +20,8 @@ npx degit KrishGarg/vite-react-tailwind-jit-template#main
 
 To start the dev server on your machine:
 
-```shell
-yarn dev
-```
+npm run dev
 
-To start the dev server on your network:
-
-```shell
-yarn dev --host
-```
-
-To make the production build:
-
-```shell
-yarn build
-```
 
 To preview the production build locally:
 
--- src/product/ProductItem.jsx
+++ src/product/ProductItem.jsx
@@ -2,7 +2,6 @@ import React,{useState,useEffect} from "react";
 import axios from "axios";
 import toast, { Toaster } from 'react-hot-toast';
 import { Link } from "react-router-dom";
-import ProductDetail from "./ProductDetail";
 
 const ProductItem = ({avatar, name, price, id}) => {
 
@@ -17,7 +16,7 @@ const ProductItem = ({avatar, name, price, id}) => {
 
 
   return (
-    <div className="px-6 pt-4 pb-2 flex flex-col w-[200px] h-[200px] mt-[120px] ml-[34px] cursor-pointer" id={id}>
+    <div className="px-6 pt-4 pb-2 flex flex-col w-[200px] h-[200px] mt-[120px] ml-[34px] cursor-pointer" key={id} id={id}>
       <Link to={`product-details/${id}`}>
       <img src={avatar} className="inline-block bg-gray-200 rounded-[20px]  text-sm font-semibold mr-2 mb-2" alt="product" />
       <span className="inline-block rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 " >{name}</span>
