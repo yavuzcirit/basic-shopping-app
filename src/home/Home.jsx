57ad9f13c146678480a2bbe3087d6adccfd0556a
yavuzcirit <yavuzcirit23@gmail.com>
2022-04-25 09:53:41 +0300

    changed css property

--- src/home/Home.jsx
+++ src/home/Home.jsx
@@ -1,55 +1,55 @@
 import axios from "axios";
-import React,{useEffect, useState} from "react";
+import React, { useEffect, useState } from "react";
 import ProductItem from "../product/ProductItem";
-import Icon from "./Icon"
-
+import Icon from "./Icon";
 
 const Home = () => {
-  const [products,setProducts] = useState([]);
+  const [products, setProducts] = useState([]);
   const [category, setCategory] = useState("");
 
   useEffect(() => {
-   async function fetchData(){
-    try {
-      let response=await axios.get('https://62286b649fd6174ca82321f1.mockapi.io/case-study/products/');
-      let myResponse = await response.data;
-      console.log(myResponse)
-      setProducts(myResponse)
-      console.log(myResponse) 
-    } catch(error){
-      console.log(error)
+    async function fetchData() {
+      try {
+        let response = await axios.get("https://62286b649fd6174ca82321f1.mockapi.io/case-study/products/");
+        let myResponse = await response.data;
+        console.log(myResponse);
+        setProducts(myResponse);
+        console.log(myResponse);
+      } catch (error) {
+        console.log(error);
+      }
     }
-   }
-   fetchData();
+    fetchData();
   }, []);
 
-  const options = ["Electronic", "Furnitures", "Clothing", "Accessories"]
+  const options = ["Electronic", "Furnitures", "Clothing", "Accessories"];
 
   return (
     <div className="flex flex-col">
       <div className="absolute top-[120px] right-[128px]">
-      <select className="w-[180px]" value={category} onChange={(e) => {
-        setCategory(e.target.value)
-      }}>
-        <option value="" disabled selected hidden>Please Choose...</option>
-        {options.map((item)=>(<option>{item}</option>))}
-      </select>
+        <select
+          className="w-[180px] mr-[30px]"
+          value={category}
+          onChange={(e) => {
+            setCategory(e.target.value);
+          }}
+        >
+          <option value="" disabled selected hidden>
+            Please Choose...
+          </option>
+          {options.map((item) => (
+            <option>{item}</option>
+          ))}
+        </select>
       </div>
-    <div className="grid grid-rows-3 grid-cols-3 gap-y-4 gap-1 mb-[400px] bg-slate-100 h-full mx-[auto]  w-full max-w-[1144px] min-h-[1750px] ml-[120px] mb-0">
-        
-
-        {category ? products
-        .filter((item)=>item.category===category)
-        .map((item)=>(
-         <ProductItem avatar={item.avatar} name={item.name} price={item.price} id={item.id} key={item.id}/>
-       )) : products
-       .map((item)=>(
-        <ProductItem avatar={item.avatar} name={item.name} price={item.price} id={item.id} key={item.id}/>
-      ))}
+      <div className="grid grid-rows-3 grid-cols-3  bg-slate-100 h-full mx-[auto]  w-full max-w-[1144px] min-h-[1750px] ml-[160px] mb-0">
+        {category
+          ? products.filter((item) => item.category === category).map((item) => <ProductItem avatar={item.avatar} name={item.name} price={item.price} id={item.id} key={item.id} />)
+          : products.map((item) => <ProductItem avatar={item.avatar} name={item.name} price={item.price} id={item.id} key={item.id} />)}
+      </div>
+      <Icon />
     </div>
-    <Icon />
-    </div>
-  )
+  );
 };
 
 export default Home;
