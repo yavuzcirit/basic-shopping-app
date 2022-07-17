69279739bd9deeeb228eccfb1f87187fedab76ae
Yavuz CIRIT <yavuz.cirit@creosafe.com>
2022-07-17 04:05:56 +0300

    added loading spinner on homepage

--- src/home/Home.jsx
+++ src/home/Home.jsx
@@ -1,11 +1,13 @@
 import axios from "axios";
 import React, { useEffect, useState } from "react";
+import Loader from "../components/Loader";
 import ProductItem from "../product/ProductItem";
 import Icon from "./Icon";
 
 const Home = () => {
   const [products, setProducts] = useState([]);
   const [category, setCategory] = useState("");
+  const [loading, setLoading] = useState(false);
 
   useEffect(() => {
     async function fetchData() {
@@ -15,6 +17,7 @@ const Home = () => {
         console.log(myResponse);
         setProducts(myResponse);
         console.log(myResponse);
+        setLoading(true);
       } catch (error) {
         console.log(error);
       }
@@ -25,32 +28,38 @@ const Home = () => {
   const options = ["Electronic", "Furnitures", "Clothing", "Accessories"];
 
   return (
-    <div className="flex flex-col">
-      <div className="absolute top-[120px] right-[128px]">
-        <select
-          title="Click to Choose Category"
-          clearable
-          className="w-[300px] mr-[100px] h-[60px] rounded-[17px] text-left p-[7px] box-shadow cursor-pointer"
-          value={category}
-          onChange={(e) => {
-            setCategory(e.target.value);
-          }}
-        >
-          <option value="" disabled selected hidden>
-            Choose Category...
-          </option>
-          {options.map((item) => (
-            <option>{item}</option>
-          ))}
-        </select>
-      </div>
-      <div className="grid grid-rows-3 grid-cols-3  bg-slate-100 h-full mx-[auto]  w-full max-w-[1144px] min-h-[1750px] ml-[300px] mb-[400px]">
-        {category
-          ? products.filter((item) => item.category === category).map((item) => <ProductItem avatar={item.avatar} name={item.name} price={item.price} id={item.id} key={item.id} />)
-          : products.map((item) => <ProductItem avatar={item.avatar} name={item.name} price={item.price} id={item.id} key={item.id} />)}
-      </div>
-      <Icon />
-    </div>
+    <>
+      {loading ? (
+        <div className="flex flex-col">
+          <div className="absolute top-[120px] right-[128px]">
+            <select
+              title="Click to Choose Category"
+              clearable
+              className="w-[300px] mr-[100px] h-[60px] rounded-[17px] text-left p-[7px] box-shadow cursor-pointer"
+              value={category}
+              onChange={(e) => {
+                setCategory(e.target.value);
+              }}
+            >
+              <option value="" disabled selected hidden>
+                Choose Category...
+              </option>
+              {options.map((item) => (
+                <option>{item}</option>
+              ))}
+            </select>
+          </div>
+          <div className="grid grid-rows-3 grid-cols-3  bg-slate-100 h-full mx-[auto]  w-full max-w-[1144px] min-h-[1750px] ml-[300px] mb-[400px]">
+            {category
+              ? products.filter((item) => item.category === category).map((item) => <ProductItem avatar={item.avatar} name={item.name} price={item.price} id={item.id} key={item.id} />)
+              : products.map((item) => <ProductItem avatar={item.avatar} name={item.name} price={item.price} id={item.id} key={item.id} />)}
+          </div>
+          <Icon />
+        </div>
+      ) : (
+        <Loader />
+      )}
+    </>
   );
 };
 
