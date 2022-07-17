6796f8e5345ff3fdd8e68f5e2d3f8e0252e32c67
Yavuz CIRIT <yavuz.cirit@creosafe.com>
2022-07-17 04:18:27 +0300

    added Loading Spinner on Detail Page

--- src/product/ProductDetail.jsx
+++ src/product/ProductDetail.jsx
@@ -1,11 +1,13 @@
 import React, { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 import axios from "axios";
+import Loader from "../components/Loader";
 
 const ProductDetail = () => {
   const { id } = useParams();
   console.log("Coming ID", id);
   const [selectedItem, setSelectedItem] = useState();
+  const [loading, setLoading] = useState(false);
 
   useEffect(() => {
     async function fetchData() {
@@ -14,6 +16,7 @@ const ProductDetail = () => {
         let product = await response.data;
         console.log(product);
         setSelectedItem(product);
+        setLoading(true);
       } catch (error) {
         console.log(error);
       }
@@ -22,17 +25,23 @@ const ProductDetail = () => {
   }, []);
 
   return (
-    <div className="flex flex-col mt-[60px] mb-[400px] ml-[180px] mr-[130px]">
-      <div className="flex mb-[30px]">
-        <img className="w-[300px] h-[250px] rounded-lg" src={selectedItem?.avatar} alt="product" />
-        <div className="flex flex-col ml-[40px]">
-          <p className="text-[30px] font-700">{selectedItem?.name}</p>
-          <p className="mt-[180px] text-[18px] font-400">$ {selectedItem?.price}</p>
+    <>
+      {loading ? (
+        <div className="flex flex-col mt-[60px] mb-[400px] ml-[180px] mr-[130px]">
+          <div className="flex mb-[30px]">
+            <img className="w-[300px] h-[250px] rounded-lg" src={selectedItem?.avatar} alt="product" />
+            <div className="flex flex-col ml-[40px]">
+              <p className="text-[30px] font-700">{selectedItem?.name}</p>
+              <p className="mt-[180px] text-[18px] font-400">$ {selectedItem?.price}</p>
+            </div>
+          </div>
+          <div className="border-t-4 border-black w-[90%] ml-[20px]"></div>
+          <p className="mt-[30px] w-[90%]">{selectedItem?.description}</p>
         </div>
-      </div>
-      <div className="border-t-4 border-black w-[90%] ml-[20px]"></div>
-      <p className="mt-[30px] w-[90%]">{selectedItem?.description}</p>
-    </div>
+      ) : (
+        <Loader />
+      )}
+    </>
   );
 };
 
